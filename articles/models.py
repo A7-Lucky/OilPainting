c02ac40795cf274ca62b7aceb74623bff4d3c257
didjsrne <hyunjae0723@gmail.com>
2022-11-22 17:32:52 +0900

    feat : 모델 추가
    - articles/Style 모델 추가
    - articles/Image 모델 추가

--- articles/models.py
+++ articles/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from users.models import User
 
 
+# 변환시킬 모델
+class Style(models.Model):
+    category = models.CharField("카테고리 이름", max_length=100)
+    
+    def __str__(self):
+        return self.category
+
+
+class Image(models.Model):
+    output_img = models.ImageField("결과사진", upload_to="output/", null=True)
+    
+    def __str__(self):
+        return self.output_img
+
 class Article(models.Model):
     user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE)
     image = models.CharField("결과사진", max_length=100)
