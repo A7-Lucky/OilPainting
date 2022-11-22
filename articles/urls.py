ef930c1b9ba2245e3cfcf96e4a7ecb4cf997dbbc
didjsrne <hyunjae0723@gmail.com>
2022-11-22 17:34:04 +0900

    fix : articles/urls.py 경로 수정
    - ArticleDetailView 생성

--- articles/urls.py
+++ articles/urls.py
@@ -4,7 +4,7 @@ from articles import views
 
 urlpatterns = [
     path("", views.ArticleView.as_view()),
-    path("<article_id>/", views.ArticleView.as_view()), 
+    path("<article_id>/", views.ArticleDetailView.as_view()), 
     path("comment/", views.CommentView.as_view()),
     path("comment/<comment_id>/", views.CommentView.as_view()),
 ] 
