ca5176f029f81342b3524778bcf183c9fbc79de4
choding91 <whdlsrjf91@naver.com>
2022-11-23 09:28:30 +0900

    feat : 테스트용 회원가입 View 추가
    - Request, Response 확인을 위한 임시 회원가입 코드 작성
    - 그 외 코드별 간단한 주석 작성
    Open #2

--- users/views.py
+++ users/views.py
@@ -2,13 +2,25 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from users.models import User
-from users.serializers import ProfileSerializer, ProfileUpdateSerializer
+from users.serializers import ProfileSerializer, ProfileUpdateSerializer, UserSerializer
 
-# Create your views here.
+
+# 테스트용 회원가입 View
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class ProfileView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
+        # .all() 사용 시 모든 유저 데이터를 받게되므로 .get() 형태로 사용
         profile = User.objects.get(id=request.user.id)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data, status=status.HTTP_200_OK)
