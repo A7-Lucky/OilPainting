1beb25c524b70b7344dd44915cebfd3c278021dd
GyuhyeonKim <112394164+9yuhyeon@users.noreply.github.com>
2022-11-25 10:06:17 +0900

    test : 회원가입, 로그인 테스트 코드 추가

--- users/tests.py
+++ users/tests.py
@@ -4,18 +4,17 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from .models import User
 
-# class SignUptest(APITestCase):
-#     def test_registrtaion(self):
-#         url = reverse("user_view")
-#         user_data = {
-#             "username" : "test",
-#             "password" : "password"
-#         }
-#         response = self.client.post(url, user_data)
-#         self.assertEqual(response.status_code, 200)
+class SignUptest(APITestCase):
+    def test_registrtaion(self):
+        url = reverse("user_view")
+        user_data = {
+            "username" : "test",
+            "password" : "password"
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.status_code, 200)
 
-
-
+        
 class LoginTest(APITestCase):
     def setUp(self):
         self.data = {'username':'testuser', 'password':'1234'}
