a588dd3930459305e3a3f91ec1f7ff2d166a3c4c
GyuhyeonKim <112394164+9yuhyeon@users.noreply.github.com>
2022-11-25 12:03:19 +0900

    refact : User 모델 변경으로 인한 테스트 코드 수정
    - User 모델의 username -> email 로 변경

--- users/tests.py
+++ users/tests.py
@@ -8,7 +8,7 @@ class SignUptest(APITestCase):
     def test_registrtaion(self):
         url = reverse("user_view")
         user_data = {
-            "username" : "test",
+            "email" : "test@test.com",
             "password" : "password"
         }
         response = self.client.post(url, user_data)
@@ -17,10 +17,9 @@ class SignUptest(APITestCase):
         
 class LoginTest(APITestCase):
     def setUp(self):
-        self.data = {'username':'testuser', 'password':'1234'}
-        self.user = User.objects.create_user('testuser', '1234')
+        self.data = {'email':'testuser@test.com', 'password':'1234'}
+        self.user = User.objects.create_user('testuser@test.com', '1234')
     
     def test_login(self):
         response = self.client.post(reverse('token_obtain_pair'), self.data)
         self.assertEqual(response.status_code, 200)
-        print(response.data['access'])
