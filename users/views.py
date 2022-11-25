be62957321fb084cea3aff2267d1e840bfb76605
GyuhyeonKim <112394164+9yuhyeon@users.noreply.github.com>
2022-11-25 09:38:47 +0900

    refeat :  중복된 코드 제거
    - 회원가입 API 중복되어 제거

--- users/views.py
+++ users/views.py
@@ -6,9 +6,8 @@ from rest_framework.generics import get_object_or_404
 from rest_framework_simplejwt.views import TokenObtainPairView
 from users.serializers import UserSerializer, CustomTokenObtainPairSerializer
 
-
+# 회원가입 API
 class UserView(APIView):
-    # 회원가입
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -17,30 +16,6 @@ class UserView(APIView):
         else:
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
+# 로그인 API
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class =  CustomTokenObtainPairSerializer
-
-
-
-
-from users.serializers import UserSerializer
-
-
-class UserView(APIView):
-    # 회원가입
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
-
-
-
-
-# 회원가입 API 작성
-# 로그인 API 작성(커스텀)
-# 소셜 로그인 API
-
